db08949cc00757973027302fc0347d06f44e288f
sergio <powerserg19@hotmail.com>
2020-05-30 06:46:23 -0400

    book ctrler

--- src/main/java/guru/springframework/spring5webapp/controller/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -1,4 +1,22 @@
 package guru.springframework.spring5webapp.controller;
 
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public getBooks(Model model){
+        return "books";
+    }
+
+
 }
